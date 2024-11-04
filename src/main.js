308282b85c67340a4654d80301af40230c5b9b12
alijradii <ali.jradi.dev@gmail.com>
2024-11-04 09:07:52 +0200

    added dynamic loading for transactions

--- src/main.js
+++ src/main.js
@@ -8,3 +8,27 @@ const transactions = [
   { note: "Rent", amount: -500, date: "2024-11-20" },
   { note: "Miscellaneous", amount: -25, date: "2024-11-25" },
 ];
+
+const renderData = () => {
+  const container = document.getElementById("transactions-container");
+
+  transactions.forEach((transaction, index) => {
+    const element = document.createElement("div");
+    element.classList.add("transaction");
+    element.setAttribute("id", `transaction-${index}`);
+    element.innerHTML = `
+      <h3 class="grow2">${transaction["note"]}</h3>
+      <h3 class="grow1">${transaction["amount"]}</h3>
+      <h3 class="grow1 text-align-right">${transaction["date"]}</h3>
+      <button class="transaction-button hidden">
+          <i class="fa fa-trash text-color-black"></i>
+      </button>
+    `;
+
+    container.appendChild(element);
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderData();
+});
