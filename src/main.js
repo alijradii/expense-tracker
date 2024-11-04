4afc88b1f8c174ad5d1a6f911e38dcf84e8f3642
alijradii <ali.jradi.dev@gmail.com>
2024-11-04 13:29:33 +0200

    added logic for removing transactions

--- src/main.js
+++ src/main.js
@@ -9,9 +9,9 @@ const transactions = [
   { note: "Miscellaneous", amount: -25, date: "2024-11-25" },
 ];
 
-const renderData = () => {
-  const container = document.getElementById("transactions-container");
-
+let removeActive = false;
+const loadTransactions = () => {
+  const transactionsList = document.getElementById("transactions-list");
   transactions.forEach((transaction, index) => {
     const element = document.createElement("div");
     element.classList.add("transaction");
@@ -20,13 +20,58 @@ const renderData = () => {
       <h3 class="grow2">${transaction["note"]}</h3>
       <h3 class="grow1">${transaction["amount"]}</h3>
       <h3 class="grow1 text-align-right">${transaction["date"]}</h3>
-      <button class="transaction-button hidden">
-          <i class="fa fa-trash text-color-black"></i>
-      </button>
     `;
 
-    container.appendChild(element);
+    const button = document.createElement("button");
+    button.setAttribute(
+      "class",
+      `transaction-button ${removeActive ? "" : "hidden"}`,
+    );
+    button.innerHTML = `<i class="fa fa-trash text-color-black"></i>`;
+    button.addEventListener("click", () => {
+      transactions.splice(index, 1);
+      transactionsList.innerHTML = "";
+      loadTransactions();
+    });
+
+    element.appendChild(button);
+
+    transactionsList.appendChild(element);
   });
+};
+
+const initActionButtons = () => {
+  const removeElementButton = document.getElementById("remove-button");
+
+  removeElementButton.addEventListener("click", () => {
+    removeActive = !removeActive;
+    removeElementButton.classList.toggle("clicked");
+    transactionButtons = Array.from(
+      document.getElementsByClassName("transaction-button"),
+    );
+
+    transactionButtons.forEach((button) => {
+      button.classList.toggle("hidden");
+    });
+  });
+
+  const expenseButton = document.getElementById("expense-button");
+  const incomeButton = document.getElementById("income-button");
+
+  const toggleActiveButton = (event) => {
+    if (event.target.classList.contains("active")) return;
+
+    expenseButton.classList.toggle("active");
+    incomeButton.classList.toggle("active");
+  };
+
+  expenseButton.addEventListener("click", toggleActiveButton);
+  incomeButton.addEventListener("click", toggleActiveButton);
+};
+
+const renderData = () => {
+  initActionButtons();
+  loadTransactions();
 };
 
 document.addEventListener("DOMContentLoaded", () => {
